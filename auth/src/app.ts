d3ef7f023cbdde8c942a625d22d00552eb65095e
roman.strazanec <strazanec.roman@gmail.com>
2021-05-14 19:39:02 +0200

    Create app

---
+++ auth/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import cookieSession from "cookie-session";
+
+import {currentUserRouter} from "./routes/current-user";
+import {singoutRouter} from "./routes/signout";
+import {singinRouter} from "./routes/signin";
+import {singupRouter} from "./routes/singup";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true,
+}));
+
+app.use(currentUserRouter);
+app.use(singupRouter);
+app.use(singoutRouter);
+app.use(singinRouter);
+
+app.all('*', async (req, res, next) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
