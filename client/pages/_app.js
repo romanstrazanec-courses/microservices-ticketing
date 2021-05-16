a8af94b3041c55ab80bf2f5ec45a4e05e7895c97
roman.strazanec <strazanec.roman@gmail.com>
2021-05-16 16:09:00 +0200

    Header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,9 +2,13 @@
 
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const AppComponent = ({Component, pageProps}) => {
-    return <Component {...pageProps}/>
+const AppComponent = ({Component, pageProps, currentUser}) => {
+    return <div>
+        <Header currentUser={currentUser}/>
+        <Component {...pageProps}/>
+    </div>;
 };
 
 AppComponent.getInitialProps = async appContext => {
