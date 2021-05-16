43eed12ad0d66a217b403fc4b282e192b6e4d422
roman.strazanec <strazanec.roman@gmail.com>
2021-05-16 15:55:26 +0200

    App's getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.css';
+// noinspection JSUnusedGlobalSymbols
 
-export default ({Component, pageProps}) => {
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+
+const AppComponent = ({Component, pageProps}) => {
     return <Component {...pageProps}/>
 };
+
+AppComponent.getInitialProps = async appContext => {
+    const client = buildClient(appContext.ctx);
+    const {data} = await client.get('/api/users/currentuser');
+    const pageProps = await appContext.Component?.getInitialProps(appContext.ctx) ?? {};
+    return {
+        pageProps,
+        ...data
+    };
+};
+
+export default AppComponent;
