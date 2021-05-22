54925feb739622bdc4aad2546d54b2026894fc35
roman.strazanec <strazanec.roman@gmail.com>
2021-05-22 10:51:28 +0200

    update ingress version

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({req}) => {
     if (typeof window === 'undefined') {
         // noinspection HttpUrlsUsage
         return axios.create({
-            baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
             headers: req.headers
         });
     } else {
