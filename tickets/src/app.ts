e77946f653225d3a872ffc6e5accd4cdff24fa30
roman.strazanec <strazanec.roman@gmail.com>
2021-05-22 12:25:56 +0200

    create new ticket

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from "cookie-session";
-import {errorHandler, NotFoundError} from "@ticketing-romanstrazanec/common";
+import {currentUser, errorHandler, NotFoundError} from "@ticketing-romanstrazanec/common";
+
+import {createTicketRouter} from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -11,6 +13,9 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }));
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res, next) => {
     throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,11 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from "mongoose";
-import request from "supertest";
-import {app} from "../app";
+import jwt from 'jsonwebtoken';
 
 declare global {
     namespace NodeJS {
         interface Global {
-            signin(): Promise<string[]>
+            signin(): string[]
         }
     }
 }
@@ -38,14 +37,13 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
-
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({email, password})
-        .expect(201);
-
-    return response.get('Set-Cookie');
+global.signin = () => {
+    return [`express:sess=${
+        Buffer.from(JSON.stringify({
+            jwt: jwt.sign({
+                id: 'dah32en',
+                email: 'test@test.com'
+            }, process.env.JWT_KEY!)
+        })).toString('base64')
+    }`];
 };
