d1d2f4c1f10fcc7e48c144c05aee8de0e43f0353
roman.strazanec <strazanec.roman@gmail.com>
2021-05-22 13:14:54 +0200

    show all tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import {currentUser, errorHandler, NotFoundError} from "@ticketing-romanstrazane
 
 import {createTicketRouter} from "./routes/new";
 import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes";
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res, next) => {
     throw new NotFoundError();
