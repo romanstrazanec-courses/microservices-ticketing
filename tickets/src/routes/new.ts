edd0b70122c9af1f370453b85ea3339dd4266d1a
roman.strazanec <strazanec.roman@gmail.com>
2021-05-23 14:31:24 +0200

    add publisher and listener

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express";
 import {body} from "express-validator";
 import {requireAuth, validateRequest} from '@ticketing-romanstrazanec/common';
 import {Ticket} from "../models/ticket";
+import {TicketCreatedPublisher} from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -19,6 +20,12 @@ router.post('/api/tickets', requireAuth, [
         title, price, userId: req.currentUser!.id
     });
     await ticket.save();
+    await new TicketCreatedPublisher(client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId
+    });
 
     res.status(201).send(ticket);
 });
