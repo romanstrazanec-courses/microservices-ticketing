afb200c81eb3b91da894caab0caeecd6a75ad4dc
roman.strazanec <strazanec.roman@gmail.com>
2021-05-23 23:38:46 +0200

    order and ticket models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,16 @@
+import mongoose from "mongoose";
 import {Request, Response, Router} from 'express';
+import {requireAuth, validateRequest} from "@ticketing-romanstrazanec/common";
+import {body} from "express-validator";
 
 const router = Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+    body('ticketId')
+        .notEmpty()
+        .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+        .withMessage('TicketId must be provided')
+], validateRequest, async (req: Request, res: Response) => {
     res.send({});
 });
 
