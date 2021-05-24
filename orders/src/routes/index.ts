8ee11485181e290770994091ed0a40d321d3f805
roman.strazanec <strazanec.roman@gmail.com>
2021-05-24 12:46:41 +0200

    orders index route

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
 import {Request, Response, Router} from 'express';
+import {requireAuth} from "@ticketing-romanstrazanec/common";
+import {Order} from "../models/order";
 
 const router = Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+    const orders = await Order.find({
+        userId: req.currentUser!.id
+    }).populate('ticket');
+
+    res.send(orders);
 });
 
 export {router as indexOrderRouter};
