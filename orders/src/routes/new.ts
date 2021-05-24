09f2020b22d7e75d092df72acb5a5fcab51566cc
roman.strazanec <strazanec.roman@gmail.com>
2021-05-24 12:21:12 +0200

    Logic for new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 import {Request, Response, Router} from 'express';
-import {requireAuth, validateRequest} from "@ticketing-romanstrazanec/common";
+import {
+    BadRequestError,
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest
+} from "@ticketing-romanstrazanec/common";
 import {body} from "express-validator";
+import {Ticket} from "../models/ticket";
+import {Order} from "../models/order";
 
 const router = Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post('/api/orders', requireAuth, [
     body('ticketId')
@@ -11,7 +21,35 @@ router.post('/api/orders', requireAuth, [
         .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
         .withMessage('TicketId must be provided')
 ], validateRequest, async (req: Request, res: Response) => {
-    res.send({});
+    const {ticketId} = req.body;
+
+    // Find the ticket the user is trying to order in the database.
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError();
+
+    // Make sure that this ticket is not already reserved.
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just fetched *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved.
+    if (await ticket.isReserved()) throw new BadRequestError('Ticket is already reserved');
+
+    // Calculate an expiration date for this order.
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database.
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket
+    });
+    await order.save();
+
+    // Publish an event saying that the order was created.
+
+
+    res.status(201).send(order);
 });
 
 export {router as newOrderRouter};
