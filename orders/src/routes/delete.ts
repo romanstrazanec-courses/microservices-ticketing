55cadbeeed9ec20b7c51060e02da07c3cb4993f5
roman.strazanec <strazanec.roman@gmail.com>
2021-05-24 13:09:08 +0200

    orders delete route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,17 @@
 import {Request, Response, Router} from 'express';
+import {NotAuthorizedError, NotFoundError, requireAuth} from "@ticketing-romanstrazanec/common";
+import {Order, OrderStatus} from "../models/order";
 
 const router = Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
 });
 
 export {router as deleteOrderRouter};
