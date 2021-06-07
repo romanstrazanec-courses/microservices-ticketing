c4dbd76c19ec43742f02dd34198d8e3f66c9cef3
roman.strazanec <strazanec.roman@gmail.com>
2021-06-07 12:20:28 +0200

    passing down current user

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,14 +7,14 @@ import Header from '../components/header';
 const AppComponent = ({Component, pageProps, currentUser}) => {
     return <div>
         <Header currentUser={currentUser}/>
-        <Component {...pageProps}/>
+        <Component currentUser={currentUser} {...pageProps}/>
     </div>;
 };
 
 AppComponent.getInitialProps = async appContext => {
     const client = buildClient(appContext.ctx);
     const {data} = await client.get('/api/users/currentuser');
-    const pageProps = await appContext.Component?.getInitialProps?.(appContext.ctx) ?? {};
+    const pageProps = await appContext.Component?.getInitialProps?.(appContext.ctx, client, data.currentUser) ?? {};
     return {
         pageProps,
         ...data
